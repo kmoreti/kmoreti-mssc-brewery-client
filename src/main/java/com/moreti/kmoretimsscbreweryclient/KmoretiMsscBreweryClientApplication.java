45f8902a5f69878097a95ba6f34d5d763e8d3a06
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-11-24 22:38:02 +0000

    Initial commit

---
+++ src/main/java/com/moreti/kmoretimsscbreweryclient/KmoretiMsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.moreti.kmoretimsscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmoretiMsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmoretiMsscBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreti/kmoretimsscbreweryclient/KmoretiMsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreti.kmoretimsscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmoretiMsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
