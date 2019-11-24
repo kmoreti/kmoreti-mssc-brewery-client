f191e6ba121eea5b6a2c8b637a41c5c69c4cfed6
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-11-24 22:59:58 +0000

    Added test to get beer by id

---
+++ src/test/java/com/moreti/kmoretimsscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.moreti.kmoretimsscbreweryclient.web.client;
+
+import com.moreti.kmoretimsscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+}
